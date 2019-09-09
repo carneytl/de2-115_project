dfb67850372ced435e865521d3922acf244d0a6b
carneytl <55037870+carneytl@users.noreply.github.com>
2019-09-08 23:10:32 -0500

    DE-Lite Example
    Lab 4

---
+++ lab04/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c ../lab04_bsp/system.h \
+ ../lab04_bsp/linker.h ../lab04_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../lab04_bsp//HAL/inc/io.h ../lab04_bsp//HAL/inc/alt_types.h
+
+../lab04_bsp/system.h:
+
+../lab04_bsp/linker.h:
+
+../lab04_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../lab04_bsp//HAL/inc/io.h:
+
+../lab04_bsp//HAL/inc/alt_types.h:
